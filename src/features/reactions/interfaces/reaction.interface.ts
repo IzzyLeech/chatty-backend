27a24cbca8a3197acccf0407cdaf243d3fc763c2
IzzyLeech <ralphhunter46@gmail.com>
2025-10-04 13:39:47 +0100

    reactions interface, modal schema and Joi schemes

---
+++ src/features/reactions/interfaces/reaction.interface.ts
@@ -0,0 +1,48 @@
+import { ObjectId } from 'mongodb';
+import { Document } from 'mongoose';
+
+export interface IReactionCache {
+   _id?: string | ObjectId;
+  username: string;
+  avatarColor: string;
+  type: string;
+  postId: string;
+  profilePicture: string;
+  createdAt?: Date;
+  userTo?: string | ObjectId;
+  comment?: string;
+}
+
+
+export interface IReactionDocument extends IReactionCache, Document {
+  _id: string | ObjectId;
+}
+
+export interface IReactions {
+  like: number;
+  love: number;
+  happy: number;
+  wow: number;
+  sad: number;
+  angry: number;
+}
+
+export interface IReactionJob {
+  postId: string;
+  username: string;
+  previousReaction: string;
+  userTo?: string;
+  userFrom?: string;
+  type?: string;
+  reactionObject?: IReactionDocument;
+}
+
+export interface IQueryReaction {
+  _id?: string | ObjectId;
+  postId?: string | ObjectId;
+}
+
+export interface IReaction {
+  senderName: string;
+  type: string;
+}
