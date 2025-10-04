25c6845b9672854c28a5cd991db5a54dd752f4e7
IzzyLeech <ralphhunter46@gmail.com>
2025-10-04 18:36:21 +0100

    remove post reaction from redis cache

--- src/shared/services/redis/post.cache.ts
+++ src/shared/services/redis/post.cache.ts
@@ -2,8 +2,9 @@ import { BaseCache } from '@service/redis/base.cache';
 import Logger from 'bunyan';
 import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
-import { ISavePostToCache, IReactions, IPost } from '@post/interfaces/post.interface';
+import { ISavePostToCache, IPost } from '@post/interfaces/post.interface';
 import { Helpers } from '@global/helpers/helpers';
+import { IReactions } from '@reaction/interfaces/reaction.interface';
 
 const log: Logger = config.createLogger('postCache');
 
--- src/shared/services/redis/reaction.cache.ts
+++ src/shared/services/redis/reaction.cache.ts
@@ -1,8 +1,11 @@
+import { IReactionCache } from './../../../features/reactions/interfaces/reaction.interface';
 import { config } from '@root/config';
 import { BaseCache } from './base.cache';
 import Logger from 'bunyan';
 import { ServerError } from '@global/helpers/error-handler';
 import { IReaction, IReactionCache } from '@reaction/interfaces/reaction.interface';
+import { Helpers } from '@global/helpers/helpers';
+import { find } from 'lodash';
 
 const log: Logger = config.createLogger('reactionsCache');
 
@@ -24,7 +27,7 @@ export class ReactionCache extends BaseCache {
       }
 
       if (previousReaction) {
-        // call remove reaction method
+        this.removePostReactionFromCache(key,reaction.username, postReactions);
       }
 
       if (type) {
@@ -36,5 +39,34 @@ export class ReactionCache extends BaseCache {
       log.error(error);
       throw new ServerError('Server error. Try again');
     }
+  }
+
+  public async removePostReactionFromCache(key: string, username: string, postReactions: IReaction): Promise<void> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const response: string[] = await this.client.LRANGE(`reactions:${key}`, 0, -1);
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      const userPreviousReaction: IReactionCache = this.getPreviosReaction(response, username) as IReactionCache;
+      multi.LREM(`REACTIONS:${key}`, 1, JSON.stringify(userPreviousReaction));
+      await multi.exex();
+
+      const dataToSave: string[] = ['reactions', JSON.stringify(postReactions)];
+      await this.client.HSET(`posts:${key}`, dataToSave);
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+
+  private getPreviosReaction(response: string[], username: string): IReactionCache | undefined {
+    const list: IReactionCache[] = [];
+    for (const item of response) {
+      list.push(Helpers.parseJson(item) as IReactionCache);
+    }
+    return find(list, (listItem: IReactionCache) => {
+      return listItem.username === username;
+    });
   }
 }
