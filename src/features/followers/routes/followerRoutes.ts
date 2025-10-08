3e6828e31ffbe72d7fbe468008e60913f29b2e9b
IzzyLeech <ralphhunter46@gmail.com>
2025-10-08 15:09:21 +0100

    created get followers controller

--- src/features/followers/routes/followerRoutes.ts
+++ src/features/followers/routes/followerRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@follower/controllers/follower-user';
 import { Remove } from '@follower/controllers/unfollow-user';
+import { Get } from '@follower/controllers/get-followers';
 
 
 class FollowerRoutes {
@@ -14,6 +15,9 @@ class FollowerRoutes {
     public routes(): Router {
         this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
         this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
+        
+        this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
+        this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
 
 
         return this.router;
