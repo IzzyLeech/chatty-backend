a4bef7a3ae00b1dc8b1a262b4b7ab0124b0c32c4
IzzyLeech <ralphhunter46@gmail.com>
2025-10-09 14:45:07 +0100

    get notification controller

--- src/features/notification/routes/notificationRoutes.ts
+++ src/features/notification/routes/notificationRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Update } from '@notification/controllers/update-notification';
 import { Delete } from '@notification/controllers/delete-notification';
+import { Get } from '@notification/controllers/get-notifications';
 
 class NotificationRoutes {
     private router: Router;
@@ -14,6 +15,8 @@ class NotificationRoutes {
         this.router.put('/notification/:notificationId', authMiddleware.checkAuthentication, Update.prototype.notification);
 
         this.router.delete('/notification/:notificationId',authMiddleware.checkAuthentication,Delete.prototype.notification);
+
+        this.router.get('/notifications',authMiddleware.checkAuthentication,Get.prototype.notifications);
 
         return this.router;
     }
